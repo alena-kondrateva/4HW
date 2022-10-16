255d77505096c41b01ae2af685ec1c26586fe752
Alena <aln.evskv.1@mail.ru>
2022-10-16 14:13:29 +0500

    add task

---
+++ task25/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число А: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int B = int.Parse(Console.ReadLine());
+
+ToDegree(A, B);
+
+void ToDegree(int numberA, int numberB)
+{
+    int result = 1;
+    for (int i = 1; i <= numberB; i++)
+    {
+        result = result * numberA;
+    }
+    Console.WriteLine(result);
+}
---
+++ task29/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 2, 4, 9 -> [1, 2, 5, 7, 19, 2, 4, 9 ]
+// 6, 1, 33, 2, 93, 15, 424, 0-> [6, 1, 33, 2, 93, 15, 424, 0]
+
+
+int[] array = new int[8];
+
+ArrayNew(array);
+
+void ArrayNew(int[] array2)
+{
+    for (int i = 0; i < array2.Length; i++)
+    {
+        array2[i] = new Random().Next(1, 1000);
+        Console.Write(array2[i] + " ");
+    }
+}
